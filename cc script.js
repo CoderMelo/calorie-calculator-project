7d19ab910a57692549339e34a265e429a6562891
Carmelo Acosta <carmeloajr1@gmail.com>
2024-04-03 21:14:25 -0400

    add If statement that checks invalidInputMatch
    You need to check if invalidInputMatch is truthy – you can do this by passing the variable directly to your if condition (without a comparison operator).

--- cc script.js
+++ cc script.js
@@ -38,6 +38,10 @@ let calories = 0:
 for (const item of list) {
   const currVal = cleanInputString(item.value);
   let invalidInputMatch = isInvalidInput(currVal);
+  if (invalidInputMatch) {
+
+  }
+
 }
 }
 
